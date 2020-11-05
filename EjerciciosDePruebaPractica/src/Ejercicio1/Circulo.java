2a57bb08a16037348ce9a6661d3d116e8473d029
Juan Femenia Quevedo <53376393+JuanPantaleonFQ@users.noreply.github.com>
2020-11-05 12:41:20 +0100

    Commit for exercise 1.

--- EjerciciosDePruebaPractica/src/Ejercicio1/Circulo.java
+++ EjerciciosDePruebaPractica/src/Ejercicio1/Circulo.java
@@ -1,6 +1,43 @@
 package Ejercicio1;
 
 public class Circulo {
+	// atributos donde x e y representan el centro del circulo.
+	private int x;
+	private int y;
+	private int radio;
 	
+	
+	//constructor:
+	public Circulo(int x, int y, int radio) {
+		this.x = x;
+		this.y = y;
+		this.radio = radio;
+		
+	}
+	
+	
+	//metodos:
+	public double area(int radio) {		
+		return (Math.PI*Math.pow(radio,2));
+		
+	}
+	
+	public void pruebaescritura(int x, int y) {
+		System.out.println("escribe: " + x + " " + y );
+	}
+	
+	public void Trasladar(int d) {
+		x += d;
+				
+	}
+	
+	
+	// To string...
+	public String toString() {
+			return " círculo con centro (" + x + ", " + y + ") y radio " + radio;
+		}
+			
+	}
+
 
 }
---
+++ EjerciciosDePruebaPractica/src/Ejercicio1/mainEjercicio1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class mainEjercicio1 {
+
+	public static void main(String[] args) {
+		
+		int inX, inY, inR, inD;
+		Circulo c;
+		Scanner in;
+					
+			// Create scanner
+			in = new Scanner(System.in);
+			
+			// Enter x
+			System.out.print("Coordenada x: ");
+			inX = in.nextInt();
+			
+			// Enter y
+			System.out.print("Coordenada y: ");
+			inY = in.nextInt();
+			
+			// Enter radius
+			System.out.print("Radio: ");
+			inR= in.nextInt();
+
+			// Create the object!
+			c = new Circulo(inX, inY, inR);
+			
+			// Print info and read data
+			System.out.println("El área del " + c + " es " + c.area());			
+			System.out.print("Desplazamiento: ");			
+			inD = in.nextInt();			
+			
+			// Moving the circle
+			c.traslada(inD);
+			
+			// Printing more info...
+			System.out.println("Resultado: " + c);
+			
+			in.close();
+	}		
+}
+
